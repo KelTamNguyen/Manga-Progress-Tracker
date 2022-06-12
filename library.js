fe0a48060db5dec6381935510968689a6a969824
Kelvin Nguyen <keltamnguyen@yahoo.com>
2022-06-11 20:34:18 -0500

    finish form modal

--- library.js
+++ library.js
@@ -4,6 +4,7 @@ var addItemBtn = document.querySelector('.add-item-btn');
 var modalBg = document.querySelector('.modal-bg');
 var closeBtn = document.querySelector('.close');
 var submitBtn = document.querySelector('#add-manga');
+let errorMsg = document.querySelector('.error-msg');
 
 // Manga Object Section //
 
@@ -63,49 +64,64 @@ addItemBtn.addEventListener('click', function(e) {
 });
 
 closeBtn.addEventListener('click', function(e) {
+    resetForm();
     modalBg.classList.remove('modal-active');
 });
 
 submitBtn.addEventListener('click', function(e) {
     e.preventDefault();
     let title = document.querySelector('#title');
-    let authors = document.querySelector('#author');
+    let author = document.querySelector('#author');
     let chapters = document.querySelector('#chapters');
     let currentChapter = document.querySelector('#currentChapter');
     let status = document.querySelector('#status');
 
-    let newManga = new Manga(title.value, authors.value, chapters.value, currentChapter.value, status.value);
-    myLibrary.push(newManga);
+    if ((title.value !== "") && (author.value !== "") && ((!isNaN(chapters.value) && !isNaN(currentChapter.value)) && (currentChapter.value <= chapters.value))) {
+        let newManga = new Manga(title.value, author.value, chapters.value, currentChapter.value, status.value);
+        myLibrary.push(newManga);
 
-    // create a new card or entry and append it to the DOM
-    let newElement = document.createElement('li');
-    newElement.textContent = newManga.info();
-    library.appendChild(newElement);
+        // create a new card or entry and append it to the DOM
+        let newElement = document.createElement('li');
+        newElement.textContent = newManga.info();
+        library.appendChild(newElement);    
 
-    // for now, the form elements will need to be cleared manually
-    // but this should not be needed once there is an actual backend
+        // for now, the form elements will need to be cleared manually
+        // but this should not be needed once there is an actual backend
+        title.value = "";
+        author.value = "";
+        chapters.value = "";
+        currentChapter.value = "";
+        status.value = "ongoing";
+        errorMsg.textContent = "";
+
+        modalBg.classList.remove('modal-active');
+    }
+    else {
+        if (currentChapter.value > chapters.value) {
+            errorMsg.textContent = '* Current Chapter is greater than Chapters';
+        }
+        else {
+            errorMsg.textContent = '* Please fill all required fields';
+        }
+    }
+});
+
+function resetForm() {
+    let title = document.querySelector('#title');
+    let author = document.querySelector('#author');
+    let chapters = document.querySelector('#chapters');
+    let currentChapter = document.querySelector('#currentChapter');
+    let status = document.querySelector('#status');
+
     title.value = "";
-    authors.value = "";
+    author.value = "";
     chapters.value = "";
     currentChapter.value = "";
     status.value = "ongoing";
-
-    modalBg.classList.remove('modal-active');
-});
-
-/*
- * TODO: create a function to check for duplicates in myLibrary and do nothing if a duplicate is found.
- *
- * Iterate through the array and compare the objects and compare to see if it already exists
- */
-function isDuplicate(item) {
-    for (manga in myLibrary) {
-        // return true if manga is the same object 
-        if ((item.title === manga.title) && (item.author == manga.author)) {
-
-        }
-    }
+    errorMsg.textContent = "";
 }
+
+// Card Component Section //
 
 /*
  * TODO: add a button on each book’s display to remove the book from the library. 
@@ -117,10 +133,13 @@ var badThinkingDiary = new Manga("Bad Thinking Diary", "Park Do-han", 7, 1, "ong
 var unlock99 = new Manga("Unlock 99 Heroines in End Times", "Mr. Two Cats", 134, 45, "ongoing");
 var oreDake = new Manga("Ore Dake ni Koakuma na Doukyuusei", "Rifuru", 8, 1, "completed");
 var married = new Manga("I Got Married to a Villain", "빡킬 (copin) / 사동 / 십삼월의새벽", 1, 1, "cancelled");
+var married2 = new Manga("I Got Married to a Villain", "빡킬 (copin) / 사동 / 십삼월의새벽", 1, 1, "cancelled");
 addToLibrary(badThinkingDiary);
 addToLibrary(unlock99);
 addToLibrary(oreDake);
 addToLibrary(married);
+// addToLibrary(married2);
 // console.log(myLibrary);
 // console.log(married.info());
 renderLibrary();
+console.log(isDuplicate(married2));
