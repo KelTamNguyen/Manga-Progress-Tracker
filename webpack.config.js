852cb4f6ad99182aaf76ffc8c63f515178247730
Tamperauture <60494720+Tamperauture@users.noreply.github.com>
2022-08-13 20:11:16 -0500

    begin webpack integration

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/library.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            }
+        ]
+    },
+    // optimization: {
+    //     runtimeChunk: 'single',
+    // },
+}
